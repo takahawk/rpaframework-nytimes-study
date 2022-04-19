f89d29228ea1672bf7c82033388a967352ef67e6
Bogdan Ovsiannikov <takahawkkun@gmail.com>
2022-04-19 16:07:40 +0300

    Add date filter

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
 from RPA.Browser.Selenium import Selenium
 
 from nyt_article_search import NYTArticleSearcher
@@ -9,4 +12,5 @@ months = 1
 
 searcher = NYTArticleSearcher()
 searcher.start_new_search(phrase)
-searcher.apply_filter(category)
+searcher.apply_category_filter(category)
+searcher.apply_date_filter(from_date=date.today() - relativedelta(months=months), to_date=date.today())
--- nyt_article_search.py
+++ nyt_article_search.py
@@ -11,13 +11,20 @@ class NYTArticleSearcher:
         self.lib.input_text("//html/body/div[1]/div[2]/div/header/section[1]/div[1]/div[2]/div/form/div/input", phrase)
         self.lib.click_element("//html/body/div[1]/div[2]/div/header/section[1]/div[1]/div[2]/div/form/button")
 
-    def apply_filter(self, filter):
+    def apply_category_filter(self, filter):
         self._apply_filter(filter,
                            "//html/body/div[1]/div[2]/main/div/div[1]/div[2]/div/div/div[2]/div/div/button",
                            "//html/body/div[1]/div[2]/main/div/div[1]/div[2]/div/div/div[2]/div/div/div/ul")
         self._apply_filter(filter,
                            "//html/body/div[1]/div[2]/main/div/div[1]/div[2]/div/div/div[3]/div/div/button",
                            "//html/body/div[1]/div[2]/main/div/div[1]/div[2]/div/div/div[3]/div/div/div/ul")
+
+    def apply_date_filter(self, from_date, to_date):
+        self.lib.click_element("//html/body/div[1]/div[2]/main/div/div[1]/div[2]/div/div/div[1]/div/div/button")
+        self.lib.click_element("//html/body/div[1]/div[2]/main/div/div[1]/div[2]/div/div/div[1]/div/div/div/ul/li[6]/button")
+        self.lib.input_text("startDate", "{:02d}/{:02d}/{}".format(from_date.month, from_date.day, from_date.year))
+        self.lib.input_text("endDate", "{:02d}/{:02d}/{}".format(to_date.month, to_date.day, to_date.year))
+        self.lib.click_element("//html/body/div[1]/div[2]/main/div/div[1]/div[2]/div/div/div[1]/div/div/button")
 
     def _apply_filter(self, filter, picker_xpath, list_xpath):
         self.lib.click_element(picker_xpath)
