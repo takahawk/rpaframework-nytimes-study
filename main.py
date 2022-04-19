1d787bdc3b5c3a23d5d10a463ca65c3e7d50bc61
Bogdan Ovsiannikov <takahawkkun@gmail.com>
2022-04-19 13:15:59 +0300

    Add search for news

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from RPA.Browser.Selenium import Selenium
 
+# TODO: put to Robocorp Cloud
+phrase = "Ukraine"
+category = ""
+months = 1
+
 lib = Selenium()
 
 lib.open_available_browser("https://www.nytimes.com/")
+lib.click_element("//html/body/div[1]/div[2]/div/header/section[1]/div[1]/div[2]/button")
+lib.input_text("//html/body/div[1]/div[2]/div/header/section[1]/div[1]/div[2]/div/form/div/input", phrase)
+lib.click_element("//html/body/div[1]/div[2]/div/header/section[1]/div[1]/div[2]/div/form/button")
