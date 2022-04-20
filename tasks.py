39fe00900c05a6b9c3db1241f2e542f965e0ff82
Bogdan Ovsiannikov <takahawkkun@gmail.com>
2022-04-20 17:16:18 +0300

    Fix date calculation

--- tasks.py
+++ tasks.py
@@ -1,6 +1,5 @@
 import os
 from datetime import date
-from dateutil.relativedelta import relativedelta
 import json
 
 from nyt_article_search import NYTArticleSearcher
@@ -16,11 +15,16 @@ def main():
         phrase = os.getenv("PHRASE") or config['phrase']
         category = os.getenv("CATEGORY") or config['category']
         months = int(os.getenv("MONTHS") or config['months'])
-        
+        if months > 0:
+            months -= 1
+
         searcher.start_new_search(phrase)
         searcher.apply_category_filter(category)
         # TODO: fix date calculation
-        searcher.apply_date_filter(from_date=date.today() - relativedelta(months=months), to_date=date.today())
+        today = date.today()
+        start_date = date(today.year, today.month - months, 1)
+
+        searcher.apply_date_filter(from_date=start_date, to_date=today)
         searcher.export_articles_to_excel(phrase, download_images=True)
     finally:
         searcher.stop()
